2ccc0e95aa731a1aa61528bf78e74bb746968cdb
meet <you@example.com>
2018-08-08 13:58:23 -0400

    hi

--- exercises/app.py
+++ exercises/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('home.html/')
 def home_page():
-    return render_template(
-    	"index.html")
+    players=["Lio Messi","David Beckham","Naymar"]
+
+    return render_template("index.html",players=players,likes_same_sport=False)
+
 
 
 if __name__ == '__main__':
